39d3c83f757a29cac4fd6c3e63d180c23958b300
Xueyuan Yin <xueyuanyin@hotmail.com>
2020-06-24 14:20:11 +0800

    Completed LK and LKH.

--- main.py
+++ main.py
@@ -1,13 +1,156 @@
-import os
+from __future__ import annotations
+# from dataclasses import dataclass
+# from typing import List, Dict, Tuple, Set
+from itertools import permutations
 import numpy as np
+import random
+from .tour import TourDoubleList as Tour
 DATA_DIR = 'data'
 
 
 class LK:
-    pass
+    def __init__(self, cost: np.array):
+        self.cost = cost  # symmetric n-by-n numpy array
+        self.size = len(cost)
+        self.max_exchange = 5
+        self.nearest_num = 5
+        # candidates store the five nearest vertices of each nodes.
+        self.candidates = {}  # self.candidates must support __getitem__
+
+    def create_test_candidates(self):
+        for i in range(self.size):
+            self.candidates[i] = [j for j in range(self.size) if j != i]
+
+    def run(self, tour: Tour):
+        """improve an initial tour by variable-exchange until local optimum."""
+        better = tour
+        cnt = 0
+        while better is not None:
+            print("---------------------------------------------\n The ", cnt, "time.")
+            print("Improved tour:", list(tour.iter_vertices()))
+            print("Improved cost:", tour.routine_cost(self.cost))
+            cnt += 1
+            tour = self.improve(tour)
+            better = tour
+        return tour
+
+    def improve(self, tour: Tour):
+        """
+        The head function to use iterable func. dfs_iter
+        """
+        # i, j = 0, 0
+        # better = self.dfs_iter(tour, [i, tour.links[i, j]], self.cost[i, tour.links[i, j]])
+        better = None
+        for i in range(tour.size):
+            for j in [0, 1]:
+                if better is not None:
+                    return better
+                better = self.dfs_iter(tour, [i, tour.links[i, j]], self.cost[i, tour.links[i, j]])
+        if better is not None:
+            return better
+        return
+
+    def dfs_iter(self, tour: Tour, seqv: list, gain: int):
+        """
+        tour: Updated tour w.r.t. SeqV
+        seqv: sequential vertices (v_0, v_1, ..., v_{2i-2}, v_{2i-1})
+        gain: partial sum to (i-1) of |x| - |y| plus |x_i|
+        """
+        v_0 = seqv[0]
+        v_2i_1 = seqv[-1]
+        # gain += self.cost[seqv[-1], seqv[-2]]
+        mycand = self.candidates[v_2i_1]
+        for v_2i in mycand:
+            if v_2i not in seqv and gain - self.cost[v_2i_1, v_2i] > 0:
+                # Decide v_{2i+1} according to (v_0, v_{2i-1}).
+                if tour.next(v_0) == v_2i_1:
+                    direction = 1
+                elif tour.prev(v_0) == v_2i_1:
+                    direction = 0
+                else:
+                    raise ValueError('Error: Not feasible (v_0, v_{2i-1}) in given tour.')
+                v_2i__1 = tour.links[v_2i, 1 - direction]
+                if v_2i__1 not in seqv:
+                    # Calculate potential gain w.r.t. edge (v_{2i+1}, v_0)
+                    del_gain = - self.cost[v_2i_1, v_2i] + self.cost[v_2i, v_2i__1]
+                    if gain + del_gain - self.cost[v_2i__1, v_0] > 0:
+                        return tour.two_optimal([v_0, v_2i_1, v_2i, v_2i__1])
+                    return self.dfs_iter(tour.two_optimal([v_0, v_2i_1, v_2i, v_2i__1]), seqv + [v_2i, v_2i__1], gain + del_gain)
+        return
+
 
 class LKH:
-    pass
+    def __init__(self, cost: np.array):
+        self.cost = cost  # symmetric n-by-n numpy array
+        self.size = len(cost)
+        self.max_exchange = 5
+        self.nearest_num = 5
+        # candidates store the five nearest vertices of each nodes.
+        self.candidates = {}  # self.candidates must support __getitem__
+        self.maxg = 0
+        self.bestseq = None
+
+    def create_test_candidates(self):
+        for i in range(self.size):
+            self.candidates[i] = [j for j in range(self.size) if j != i]
+
+    def run(self, tour: Tour):
+        """improve an initial tour by variable-exchange until local optimum."""
+        cnt = 0
+        better = None
+        while tour is not None:
+            better = tour
+            print("---------------------------------------------\n The ", cnt, "time.")
+            print("Improved tour:", list(better.iter_vertices()))
+            print("Improved cost:", better.routine_cost(self.cost))
+            cnt += 1
+            tour = self.improve(better)
+        return better
+
+    def improve(self, tour: Tour):
+        """
+        The head function to use iterable func. dfs_iter
+        """
+        # i, j = 0, 0
+        # better = self.dfs_iter(tour, [i, tour.links[i, j]], self.cost[i, tour.links[i, j]])
+        for i in range(tour.size):
+            for j in [0, 1]:
+                better = self.dfs_iter(tour, [i, tour.links[i, j]], self.cost[i, tour.links[i, j]])
+                if better is not None:
+                    return better
+        return
+
+    def dfs_iter(self, tour: Tour, seqv: list, gain: int):
+        """
+        tour: Original tour without updation.
+        seqv: sequential vertices (v_0, v_1, ..., v_{2i-2}, v_{2i-1})
+        gain: partial sum to (i-1) of |x| - |y| plus |x_i|
+        """
+        v_0 = seqv[0]
+        v_2i_1 = seqv[-1]
+        k = len(seqv) // 2
+        mycand = self.candidates[v_2i_1]
+        for v_2i in mycand:
+            if v_2i not in seqv and gain - self.cost[v_2i_1, v_2i] > 0:
+                # Choose v_{2i+1} from neighbours of v_{2i-1}.
+                for v_2i__1 in tour.links[v_2i]:
+                    if v_2i__1 not in seqv:
+                        # Calculate potential gain w.r.t. edge (v_{2i+1}, v_0)
+                        del_gain = - self.cost[v_2i_1, v_2i] + self.cost[v_2i, v_2i__1]
+                        if k % self.max_exchange == 0:
+                            if tour.check_feasible(seqv + [v_2i, v_2i__1]):
+                                if gain + del_gain - self.cost[v_2i__1, v_0] > 0:
+                                    return tour.k_optimal(seqv + [v_2i, v_2i__1])
+                                else:
+                                    return self.dfs_iter(tour, seqv + [v_2i, v_2i__1], gain + del_gain)
+                        else:
+                            if gain + del_gain - self.cost[v_2i__1, v_0] > 0:
+                                if tour.check_feasible(seqv + [v_2i, v_2i__1]):
+                                    return tour.k_optimal(seqv + [v_2i, v_2i__1])
+                            else:
+                                return self.dfs_iter(tour, seqv + [v_2i, v_2i__1], gain + del_gain)
+        return
+
 
 if __name__ == '__main__':
     cost_filepath = 'data/ch130.npy'
--- tour.py
+++ tour.py
@@ -2,33 +2,8 @@ from typing import Sequence
 import numpy as np
 
 
-def is_permutation_n(a_seq: Sequence, p0: Sequence = None):
-    """Check whether a seq is a permutaion of range(n)."""
-    if p0 is None:
-        n = len(a_seq)
-        attendence = np.zeros(n, bool)
-        for x in a_seq:
-            try:
-                dao = attendence[x]
-            except IndexError:
-                print(f"{x} not in range({n})")
-                return False
-            else:
-                if dao:
-                    print(f"{x} is repeated")
-                    return False
-                attendence[x] = True
-        assert attendence.all(), "Do I need this line?"
-        return True
-    attendence = {x: False for x in p0}
-    for x in a_seq:
-        dao = attendence[x]
-        if dao:
-            return False
-        attendence[x] = True
-
 class TourDoubleList:
-    """Circular double linked list implementation of tour."""
+    """Doubly circular linked list implementation of tour."""
 
     def __init__(self, route: Sequence):
         """Accept any sequence of permutation of range(n). """
@@ -42,9 +17,14 @@ class TourDoubleList:
         else:
             # convert the sequence to doubly linked list.
             # self.links[i, j] is the predecessor of i if j is 0, successor of i if j is 1
-            self.links = np.zeros((self.size, 2), dtype=int)
-            for i in range(self.size):
-                self.links[route[i]] = route[i - 1], route[i]
+            self.links = np.zeros((self.size, 2), int)
+            last = route[0]
+            for curr in route[1:]:
+                self.links[last, 1] = curr
+                self.links[curr, 0] = last
+                last = curr
+            self.links[last, 1] = route[0]
+            self.links[route[0], 0] = last
 
     # def clone(self):
     #     """Copy the tour to a new one"""
@@ -81,15 +61,129 @@ class TourDoubleList:
 
     def check_feasible(self, v):
         """Walk around the tour, O(n) complexity"""
+        """
+        v: sequential series, [v_0, v_1, ..., V_2k-2, v_2k-1]
+        """
+        l = len(v)
+        if len(set(v)) != l:
+            raise ValueError("Repeat elements in sequential series!")
+        if l < 4 or l % 2 != 0:
+            raise ValueError("The number of sequential series should be an odd, at least 4!")
+        cnt = 0
+        start = 2
+        curr = 2
+        direction = int(self.links[v[curr], 1] == v[curr + 1])
+        while cnt <= self.size + 1:
+            curr = self.links[v[curr], 1 - direction]  # curr is a position
+            cnt += 1
+            while curr not in v:
+                curr = self.links[curr, 1 - direction]  # curr is a position
+                cnt += 1
+            if v.index(curr) % 2 == 1:
+                curr = (v.index(curr) + 1) % l  # curr is an index
+                direction = int(self.links[v[curr], 1] == v[curr + 1])  # now curr is odd
+            else:
+                curr = (v.index(curr) - 1) % l  # curr is an index
+                direction = int(self.links[v[curr], 1] == v[curr - 1])  # now curr is odd
+            cnt += 1
+            if curr == start:
+                break
+        if cnt == self.size:
+            return True
+        else:
+            return False
+
+    def k_optimal(self, v: list):
+        """
+        Make a k-exchange w.r.t. sequential mv = [v_0, v_1, ..., v_2k-2, v_2k-1],
+        where we add (v_2i-1, v_2i) and remove (v_2i, v_2i+1).
+        ::type mv:: length-4 list.
+        ::rtype:: Return a new Tour object, or None."""
+        l = len(v)
+        if len(set(v)) != l:
+            raise ValueError("Repeat elements in sequential series!")
+        if l < 4 or l % 2 != 0:
+            raise ValueError("The number of sequential series should be an odd, at least 4.")
+        newone = TourDoubleList([0 for i in range(self.size)])
+        cnt = 0
+        start = 2
+        curr = 2
+        direction = int(self.links[v[curr], 1] == v[curr + 1])
+        while True:
+            prev = v[curr]
+            curr = self.links[v[curr], 1 - direction]  # curr is a position
+            newone.links[prev, 1], newone.links[curr, 0] = curr, prev
+            cnt += 1
+            while curr not in v:
+                prev = curr
+                curr = self.links[curr, 1 - direction]  # curr is a position
+                newone.links[prev, 1], newone.links[curr, 0] = curr, prev
+                cnt += 1
+            prev = curr
+            if v.index(curr) % 2 == 1:
+                curr = (v.index(curr) + 1) % l  # curr is an index
+                direction = int(self.links[v[curr], 1] == v[curr + 1])  # now curr is odd
+            else:
+                curr = (v.index(curr) - 1) % l  # curr is an index
+                direction = int(self.links[v[curr], 1] == v[curr - 1])  # now curr is odd
+            newone.links[prev, 1], newone.links[v[curr], 0] = v[curr], prev
+            cnt += 1
+            if curr == start:
+                break
+        return newone
+
+    def two_optimal(self, mv: list):
+        """
+        Make a two-exchange w.r.t. edge (v_0, v_1) = (mv[0], mv[1]),  (v_2, v_3) = (mv[2], mv[3]) (to be removed)
+        and edge (v_1, v_2), (v_3, v_0) (to be added).
+        ::type mv:: length-4 list.
+        ::rtype:: Return a new Tour object, or None.
+        """
+        v0 = mv[0]
+        v1 = mv[1]
+        if self.next(v0) == v1:
+            direction = 1
+        elif self.prev(v0) == v1:
+            direction = 0
+        else:
+            raise ValueError('Error: Only move the edge between neighbor nodes.')
+        v2 = mv[2]
+        v3 = mv[3]
+        if v3 != self.links[v2, 1 - direction]:
+            raise ValueError('Error: Unfeasible position of the second vertice to be broken.')
+        # Get rid of bad cases.
+        # if v2 == -1 and v3 == -1:
+        #     return
+        if v2 == v0 or v2 == v1 or v3 == v0 or v3 == v1:
+            return
+        newone = TourDoubleList([0 for i in range(self.size)])
+        # print("Clone one:", list(newone.iter_vertices()))
+        curr = v3
+        # TicToc.tic()
+        """Found by Gao. Exchange prev. and next of ref. vertices."""
+        while curr != v0:
+            newone.links[curr, 0], newone.links[curr, 1] = self.links[curr, 1], self.links[curr, 0]
+            curr = self.links[curr, 1 - direction]
+        while curr != v3:
+            newone.links[curr] = self.links[curr]
+            curr = self.links[curr, 1 - direction]
+        # TicToc.toc()
+        newone.links[v1, direction] = v2
+        newone.links[v2, 1 - direction] = v1
+        newone.links[v0, direction] = v3
+        newone.links[v3, 1 - direction] = v0
+        # print("Original one:", list(self.iter_vertices()))
+        # print("Changed Clone one:", list(newone.iter_vertices()))
+        return newone
 
     def routine_cost(self, cost: np.array):
         """return the cost of this tour"""
         start = 0
-        next = self.links[start, 1]
-        a = cost[start, next]
-        curr = next
+        nnext = self.links[start, 1]
+        a = cost[start, nnext]
+        curr = nnext
         while curr != start:
-            next = self.links[curr, 1]
-            a += cost[curr, next]
-            curr = next
+            nnext = self.links[curr, 1]
+            a += cost[curr, nnext]
+            curr = nnext
         return a
